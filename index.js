1cc6ebb9407076a9a0c098052ee857a32c114427
Khachornchit Songsaen <khachornchit.songsaen@gmail.com>
2019-09-11 13:17:13 +0700

    update

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3001;
 
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+})
+
 const customers = [
     { id: 1, name: 'Lisa' },
     { id: 2, name: 'Joe' },
@@ -75,7 +79,3 @@ function validateCustomer(customer) {
 
     return Joi.validate(customer, schema);
 }
-
-app.listen(port, () => {
-    console.log(`Listening on ${port}`);
-})
